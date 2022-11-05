0365ac6ac9e038d08f71c4dfc86377ad134e5b7b
Caleb-Campbell <calebcampbellcrm@gmail.com>
2022-11-04 18:17:55 -0600

    db accepting migrations and rollbacks

--- api/project/model.js
+++ api/project/model.js
@@ -5,4 +5,12 @@ async function find() {
     const rows = await db('projects as p')
     .leftJoin('tasks as t', 'p.project_id', 't.project_id')
     .leftJoin('projects_resources as pr', 't.task_id', 'pr.task_id')
+    .leftJoin('resources as r', 'pr.resource_id', 'r.resource_id')
+    .select('*')
+    .orderBy(['p.project_id', 't.task_id'])
+}
+
+module.exports = {
+    find,
+    
 }
