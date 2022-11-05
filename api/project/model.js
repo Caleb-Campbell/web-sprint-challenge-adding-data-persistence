26d6ad042f8b6a88977fb8f02219f6564067913d
Caleb-Campbell <calebcampbellcrm@gmail.com>
2022-11-04 18:09:07 -0600

    server and routers up and running

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,8 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig')
+
+async function find() {
+    const rows = await db('projects as p')
+    .leftJoin('tasks as t', 'p.project_id', 't.project_id')
+    .leftJoin('projects_resources as pr', 't.task_id', 'pr.task_id')
+}
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,10 @@
 // build your `/api/resources` router here
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.status(200).json({message: 'hey its working'})
+})
+
+module.exports = router
